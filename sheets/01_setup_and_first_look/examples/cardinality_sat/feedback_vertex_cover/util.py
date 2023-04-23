3e8babdd408f24115af7ce81568860ec579d0e6f
Gabriel Gehrke <g.gehrke@tu-bs.de>
2023-04-23 15:32:10 +0200

    changed fvs visualization to more aesthetically pleasing colors

--- sheets/01_setup_and_first_look/examples/cardinality_sat/feedback_vertex_cover/util.py
+++ sheets/01_setup_and_first_look/examples/cardinality_sat/feedback_vertex_cover/util.py
@@ -41,9 +41,9 @@ def visualize_fvs(graph: nx.Graph, feedback_vertex_set: Set[Node]):
     Draws a 'before-after' visualization of a given Feedback Vertex Set solution on a given graph.
     """
     layout = nx.layout.kamada_kawai_layout(graph)
-    nx.draw_networkx(graph, pos=layout, node_color=["red" if p in feedback_vertex_set else "cyan" for p in graph.nodes()])
+    nx.draw_networkx(graph, pos=layout, node_color=["red" if p in feedback_vertex_set else "grey" for p in graph.nodes()])
     plt.show()
     _graph = graph.copy()
     _graph.remove_nodes_from(feedback_vertex_set)
-    nx.draw_networkx(_graph, pos=layout, node_color="cyan")
+    nx.draw_networkx(_graph, pos=layout, node_color="grey")
     plt.show()
