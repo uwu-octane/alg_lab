38fba322e318c0f5d0f7c1f99ab2fc063dccd31c
fabian <fpfitzner@pfsense.home>
2023-06-12 15:25:57 +0200

    Adjusted solver.py for MIP

--- 04_mip/MIP/solver.py
+++ 04_mip/MIP/solver.py
@@ -5,7 +5,7 @@ from networkx.classes.graphviews import subgraph_view
 import math
 
 
-class DBSTSolverIP:
+class BTSPSolverIP:
     def __make_vars(self):
         # Create binary variables for every *undirected* edge
         self.bnvars = {e: self.model_bottleneck.addVar(lb=0, ub=1, vtype=grb.GRB.BINARY) for e in self.all_edges}
@@ -24,13 +24,13 @@ class DBSTSolverIP:
                 if e in varmap:
                     edgevars += varmap[e]
             model.addConstr(edgevars >= 1)
-            model.addConstr(edgevars <= self.degree)
+            model.addConstr(edgevars <= 2)
 
     def __add_total_edges(self, model, varmap: dict):
         """
-        Enforce the constraint sum(x_e) = n-1
+        Enforce the constraint sum(x_e) = n
         """
-        model.addConstr(sum(varmap.values()) == len(self.points) - 1)
+        model.addConstr(sum(varmap.values()) == len(self.points))
 
     def __make_edges(self):
         edges_of = {p: list() for p in self.points}
@@ -111,12 +111,9 @@ class DBSTSolverIP:
     def __init__(self, points, edges, degree):
         self.points = points
         self.all_edges = edges
-        self.degree = degree
         self.edges_of = self.__make_edges()
         self.model_bottleneck = grb.Model()  # "First stage" model for finding the bottleneck edge
-        self.model_minsum = grb.Model()  # "Second stage" model for finding the cost-minimal DBST with fixed bottleneck.
         self.remaining_edges = None
-        self.msvars = None
         self.__make_vars()
         self.__add_degree_bounds(self.model_bottleneck, self.bnvars)
         self.__add_total_edges(self.model_bottleneck, self.bnvars)
@@ -125,20 +122,6 @@ class DBSTSolverIP:
         self.model_bottleneck.Params.lazyConstraints = 1
         # Set the first objective
         self.model_bottleneck.setObjective(self.l, grb.GRB.MINIMIZE)
-
-    def __init_minsum_model(self):
-        """
-        Set up variables, constraints and objective function for the
-        second stage model.
-        """
-        # Create binary variables (vtype=grb.GRB.BINARY) for all edges
-        self.msvars = {e: self.model_minsum.addVar(lb=0, ub=1, vtype=grb.GRB.BINARY)
-                       for e in self.remaining_edges}
-        self.__add_degree_bounds(self.model_minsum, self.msvars)
-        self.__add_total_edges(self.model_minsum, self.msvars)
-        self.model_minsum.Params.lazyConstraints = 1
-        obj = sum((math.dist(*e) * x_e for e, x_e in self.msvars.items()))
-        self.model_minsum.setObjective(obj, grb.GRB.MINIMIZE)
 
     def __solve_bottleneck(self):
         # Find the optimal bottleneck (first stage)
@@ -151,20 +134,5 @@ class DBSTSolverIP:
         self.remaining_edges = [e for e in self.all_edges if math.dist(*e) <= bottleneck]
         return [e for e, x_e in self.bnvars.items() if x_e.x >= 0.5]
 
-    def __solve_minsum(self):
-        # Find the optimal tree (second stage)
-        self.__init_minsum_model()
-        cb_ms = lambda model, where: self.callback(where, model, self.msvars)
-        self.model_minsum.optimize(cb_ms)
-        if self.model_bottleneck.status != grb.GRB.OPTIMAL:
-            raise RuntimeError("Unexpected status after optimization!")
-        # Return all edges with value >= 0.5 (numerical reasons)
-        print(f"[DBST SOLVER]: Found the optimal tree! Total cost: {self.model_minsum.objVal}")
-        return [e for e, x_e in self.msvars.items() if x_e.x >= 0.5]
-
     def solve(self, minsum: bool = False):
         dbst_edges = self.__solve_bottleneck()
-        if not minsum:
-            return dbst_edges
-        else:
-            return self.__solve_minsum()
