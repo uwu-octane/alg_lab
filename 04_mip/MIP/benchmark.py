c10fe37b9c3d94463b7cf4308424bf8d2c4d5de1
Zorgosto <zorgosto3@gmail.com>
2023-06-13 19:51:02 +0200

    add greedy

--- 04_mip/MIP/benchmark.py
+++ 04_mip/MIP/benchmark.py
@@ -1,15 +1,20 @@
 from MIP import *
 import statistics
+import random
 
 
-def benchmark_average_time(num_points, iterations):
+def benchmark_average_time(num_points, iterations, use_greedy=False):
     times = list()
     for i in range(iterations):
         points = util.random_points(num_points)
         edges = util.all_edges_sorted(points)
         solver = BTSPSolverIP(points, edges)
         solver.model_bottleneck.setParam('LogToConsole', 0)
-        sol, time_taken = solver.solve()
+        if use_greedy:
+            start, bottleneck = Greedy_Btsp(points).solve()
+            sol, time_taken = solver.solve(start, bottleneck)
+        else:
+            sol, time_taken = solver.solve()
         times.append(time_taken)
     average_time = statistics.mean(times)
     print(f'Average time: {average_time}')
@@ -26,8 +31,10 @@ def benchmark_time_limit(time_limit, start_points, step):
         edges = util.all_edges_sorted(points)
         solver = BTSPSolverIP(points, edges)
         solver.model_bottleneck.setParam('TimeLimit', time_limit)
+        solver.model_bottleneck.setParam('LogToConsole', 0)
         try:
             last_solvable_instance, time_taken = solver.solve()
+            print(f'Time taken for {num_points} points: {time_taken}')
         except TimeoutError as e:
             print(e)
             break
@@ -39,5 +46,9 @@ def benchmark_time_limit(time_limit, start_points, step):
     print(f'First unsolvable instance has {num_points} points:')
 
 
-# benchmark_time_limit(10, 50, 10)
-benchmark_average_time(50, 10)
+# benchmark_time_limit(300, 205, 5)
+random.seed('123456')
+benchmark_average_time(num_points=100, iterations=5, use_greedy=False)
+print('############################')
+random.seed('123456')
+benchmark_average_time(num_points=100, iterations=5, use_greedy=True)
