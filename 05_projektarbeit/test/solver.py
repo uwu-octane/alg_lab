87529d1e79a3d06044a5a2fdd8f07ad30fcc5875
fabian <fpfitzner@pfsense.home>
2023-06-30 16:51:36 +0200

    Refactored constraines and implemented solve method

--- 05_projektarbeit/test/solver.py
+++ 05_projektarbeit/test/solver.py
@@ -6,8 +6,9 @@ import networkx as nx
 class GameSolver:
     
     def __make_vars(self):
-        self.vars = {n : tuple(self.model.NewBoolVar(f'{n}_{i}') for i in range(self.k)) for n in nx.nodes(self.graph)}
+        self.vars = {n : tuple(self.model.NewBoolVar(f'x_{i}') for i in range(self.k)) for n in list(self.graph)}
 
+        # Start points which have to be connected
         for i in range(self.k):
             self.vars[start[i][0]] = 1
             self.vars[start[i][1]] = 1
@@ -17,7 +18,8 @@ class GameSolver:
         Enforce that each node can only be once
         """ 
         for v in nx.nodes(self.graph):
-            self.model.Add(sum(self.vars[v]) == 1)
+            self.model.Add(cp_model.LinearExpr.Sum(self.vars[v]) == 1)
+            #self.model.Add(cp_model.LinearExpr.Sum(cp_model.NewIntVar(self.vars[v][i]) for i in range(len(self.vars[v]))) == 1)
 
     def __connectivity_constraint(self):
         """ 
@@ -31,9 +33,14 @@ class GameSolver:
         """ 
         Enfore degree on each node
         """ 
-        for v in nx.nodes(self.graph):
-            for i in range(self.k):
-                self.model.Add(sum(self.vars[w][i] for w in nx.all_neighbors(self.graph,v)) == 2)
+        for v in list(self.graph):
+            # Check if v is a start node
+            if v not in [n for t in self.start for n in t]:
+                for i in range(self.k):
+                    self.model.Add(cp_model.LinearExpr.Sum(self.vars[w][i] for w in nx.all_neighbors(self.graph,v)) == 2)
+            else:
+                for i in range(self.k):
+                    self.model.Add(cp_model.LinearExpr.Sum(self.vars[w][i] for w in nx.all_neighbors(self.graph,v)) == 1)
 
         
     def __init__(self, graph, start):
@@ -41,16 +48,26 @@ class GameSolver:
         self.start = start
         self.k = len(start)
         self.model = cp_model.CpModel()
-
-    def solve(self):
         self.__make_vars()
         self.__single_selection_constraint()
         self.__connectivity_constraint()
         self.__degree_constraint()
-        
+
+    def solve(self):
+        solver = self.model.CpSolver()
+        status = solver.Solve(self.model)
+        if status == self.model.INFEASIBLE:
+            raise RuntimeError("The model was classified infeasible by the solver!")
+        if status != self.model.OPTIMAL:
+            raise RuntimeError("Unexpected status after running solver!") 
+
+        return [n for n,b in self.vars.items if solver.Value(b) != 0]
+
+
 
 
 G = nx.grid_2d_graph(5, 5)
-start = [(G.nodes[1], G.nodes[21])]
+start = [(list(G)[1], list(G)[21])]
 solver = GameSolver(G, start)
+print(solver.solve())
 
