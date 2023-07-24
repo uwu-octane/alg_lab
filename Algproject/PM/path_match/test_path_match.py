ab8dcfe1b90c9a88e91b51968ccf99d10763142a
tingting tao <tingting.tao@tu-braunschweig.de>
2023-07-24 16:29:44 +0200

    color

--- Algproject/PM/path_match/test_path_match.py
+++ Algproject/PM/path_match/test_path_match.py
@@ -12,9 +12,9 @@ def test_gen_start_points():
     print(list(start[0])[1])
 
 
-def test_solver(num_points, width=10, height=10):
-    G = gen_grid(width, height)
-    start = gen_start_points(num_points, G)
+def test_solver():
+    G = gen_grid(5, 10)
+    start = gen_start_points(20, G)
     #start = [(0, 1), (1, 1)]
     #start = [(0, 0), (3,0), (0,1),(3,1),(0,2),(3,2),(0,3),(3,3)]
     #print(start[1])
@@ -34,7 +34,8 @@ def test_solver(num_points, width=10, height=10):
     #print(paths))
     #print(start[1])
    # draw_result_edges(edges, G)
-    draw_result_colorful(edges, paths, G)
+
+    draw_result_colorful(edges, paths, G, False)
 
 
 def test_solver_constarint():
@@ -45,7 +46,7 @@ def test_solver_constarint():
 
 
 class MyTestCase(unittest.TestCase):
-    test_solver(num_points=10, width=5, height=10)
+    pass
 
 
 if __name__ == '__main__':
--- Algproject/PM/path_match/util.py
+++ Algproject/PM/path_match/util.py
@@ -1,8 +1,10 @@
+import colorsys
 import random
 from typing import List, Set, Tuple, Iterable, Optional
 import matplotlib.pyplot as plt
 import networkx as nx
 import itertools
+import matplotlib.colors as mcolors
 
 Node = Tuple[int, int]
 Edge = Tuple[Node, Node]
@@ -31,10 +33,48 @@ def gen_grid(grid_width, grid_height):
 
 
 def generate_random_rgb():
-    r = random.random()
-    g = random.random()
-    b = random.random()
-    return r, g, b
+    # color = random.choice(list(mcolors.CSS4_COLORS.keys()))
+    # print(color)
+    # 获取所有颜色及其对应的RGB值
+    colors_with_rgb = mcolors.CSS4_COLORS
+    color = random.choice(list(mcolors.CSS4_COLORS.values()))
+    # 打印所有颜色及其对应的RGB值
+    print(color)
+
+    return color
+
+
+def is_bright_color(hex_color, threshold=128):
+    hex_color = hex_color.lstrip("#")
+    red = int(hex_color[0:2], 16)
+    green = int(hex_color[2:4], 16)
+    blue = int(hex_color[4:6], 16)
+
+    # 计算亮度（Luminance）
+    luminance = (0.2126 * red) + (0.7152 * green) + (0.0722 * blue)
+
+    # 根据给定的阈值判断是否为亮色
+    return luminance >= threshold
+
+
+def is_clear_color(hex_color, brightness_threshold=0.4, saturation_threshold=0.3):
+    hex_color = hex_color.lstrip("#")
+    red = int(hex_color[0:2], 16)
+    green = int(hex_color[2:4], 16)
+    blue = int(hex_color[4:6], 16)
+
+    # 将RGB颜色转换为HSL颜色模式
+    h, l, s = colorsys.rgb_to_hls(red / 255.0, green / 255.0, blue / 255.0)
+
+    # 检查亮度和饱和度是否符合条件
+    return brightness_threshold <= l <= 0.5 and saturation_threshold <= s <= 0.5
+
+
+def generate_random_rgb_from_hex():
+    while True:
+        color = generate_random_rgb()
+        if is_clear_color(color):
+            return color
 
 
 def draw_result_colorful(edges, paths, grid_graph, with_laber=False):
@@ -53,7 +93,7 @@ def draw_result_colorful(edges, paths, grid_graph, with_laber=False):
         """
         for every path generate a random color
         """
-        color = generate_random_rgb()
+        color = generate_random_rgb_from_hex()
         nx.draw_networkx_edges(grid_graph, pos, edgelist=path, edge_color=color, width=2.0)
 
         """
