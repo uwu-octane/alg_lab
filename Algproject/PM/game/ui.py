9bf8df3ca758557e6b4eba49dcbccd4fa1a9a680
fabian <fpfitzner@pfsense.home>
2023-08-24 13:37:34 +0200

    remove random pair checkbox
    - there was no use case use case
    - could be implemented again iff there is a way to add pairs by the user

--- Algproject/PM/game/ui.py
+++ Algproject/PM/game/ui.py
@@ -16,10 +16,6 @@ class Ui:
         self.tp_width = tp.Labelled("Cells Width:", self.tp_width_input)
         self.tp_height_input = tp.TextInput("", "Type text here")
         self.tp_height = tp.Labelled("Cells Height:", self.tp_height_input)
-        # self.tp_height = tp.Labelled("Cells Height:", tp.TextInput("", "Type text here"))
-        self.tp_checkbox_random = tp.Labelled("Random pairs?", tp.Checkbox())
-        self.tp_checkbox_random.element._at_click = self.__checkbox_random_callback
-        self.tp_checkbox_random.element.set_value(True)
         self.tp_amount_pairs_input = tp.TextInput("", "Type text here")
         self.tp_amount_pairs = tp.Labelled("Amount of pairs:", self.tp_amount_pairs_input)
         self.tp_checkbox_bottleneck = tp.Labelled("Bottleneck?", tp.Checkbox())
@@ -35,7 +31,7 @@ class Ui:
         self.tp_button_check = tp.Button("Check")
         self.tp_button_group = tp.Group([self.tp_button_apply, self.tp_button_clear, self.tp_button_solve, self.tp_button_check], "h")
 
-        self.tp_instance_box = tp.TitleBox("Instance Settings", [self.tp_width, self.tp_height, self.tp_checkbox_random, self.tp_amount_pairs, self.tp_checkbox_bottleneck], sort_immediately=True)
+        self.tp_instance_box = tp.TitleBox("Instance Settings", [self.tp_width, self.tp_height, self.tp_amount_pairs, self.tp_checkbox_bottleneck], sort_immediately=True)
 
         self.tp_solution_box = tp.TitleBox("Solution", [self.tp_bottleneck, self.tp_valid], sort_immediately=True)
 
@@ -44,12 +40,6 @@ class Ui:
 
         self.updater = self.tp_ui_box.get_updater()
         self.events = None
-
-    def __checkbox_random_callback(self):
-        if self.tp_checkbox_random.get_value():
-            self.tp_amount_pairs.set_locked(True)
-        else:
-            self.tp_amount_pairs.set_locked(False)
 
     def __checkbox_bottleneck_callback(self):
         if self.tp_checkbox_bottleneck.get_value():
